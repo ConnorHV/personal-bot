ac609764e9ebfe1d703d8478f8cb4df5b983fd6a
ConnorHV <connorverkuilen@gmail.com>
2024-02-09 04:36:04 +0000

    Added 2 slash commands

--- src/register-command.js
+++ src/register-command.js
@@ -2,26 +2,32 @@ require('dotenv').config();
 const { REST, Routes } = require('discord.js');
 
 const commands = [
-    {
-        name: 'Hey',
-        description: 'Replies with hey!',
-    },
+  {
+    name: 'hey',
+    description: 'Replies with hey!',
+  },
+  {
+    name: 'ping',
+    description: 'Pong!',
+  },
 ];
 
-const rest = new REST({version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-    try {
-        console.log('Registering slash commands...');
+  try {
+    console.log('Registering slash commands...');
 
-        await rest.put(
-            Routes.applicationGuildCommand(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands }
-        );
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
 
-        console.log('Slash commands were registered successfully!');
-    } catch (error) {
-        console.log(`There was an error: ${error}`);
-    }
-
+    console.log('Slash commands were registered successfully!');
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+  }
 })();
