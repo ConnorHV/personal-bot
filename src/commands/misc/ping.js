c485abdb1c96b52a8b481e9e40b68342c2ee7aad
ConnorHV <connorverkuilen@gmail.com>
2024-02-27 18:44:40 +0000

    Updated ping command to test server only

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'ping',
     description: 'Pong!',
     devOnly: true,
-    // testOnly: Boolean,
-    // options: Object[],
-    // deleted: Boolean,
-
+    testOnly: true,
+    //options: Object[],
+    //deleted: Boolean,
+  
     callback: (client, interaction) => {
-        interaction.reply(`Pong! ${client.ws.ping}ms`);
+      interaction.reply(`Pong! ${client.ws.ping}ms`);
     },
-};
+  };
